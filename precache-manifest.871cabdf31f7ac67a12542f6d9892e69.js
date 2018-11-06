68867b12113c8bc4792efbf800aa29d9969d20f6
henriquejo <henriquejacomini12@gmail.com>
2018-11-06 14:18:08 -0200

    Updates

---
+++ precache-manifest.871cabdf31f7ac67a12542f6d9892e69.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee12b8452a45a32954b8efd9dcc83e2d",
+    "url": "/trabalho2/static/media/facebook.ee12b845.png"
+  },
+  {
+    "revision": "fd0ebde6e34a6631c57d",
+    "url": "/trabalho2/static/css/main.807d244f.chunk.css"
+  },
+  {
+    "revision": "49c8eb7be7fd7e73b368",
+    "url": "/trabalho2/static/js/1.49c8eb7b.chunk.js"
+  },
+  {
+    "revision": "abd34c343a8dd97c6852",
+    "url": "/trabalho2/static/js/runtime~main.abd34c34.js"
+  },
+  {
+    "revision": "283e329c9941df1d7dcfe4d1e2ae0965",
+    "url": "/trabalho2/static/media/artigo1.283e329c.jpg"
+  },
+  {
+    "revision": "fb529b7886fafc97d620674029e04feb",
+    "url": "/trabalho2/static/media/artigo2.fb529b78.jpg"
+  },
+  {
+    "revision": "df785af0588f794a575e8f15c4217751",
+    "url": "/trabalho2/static/media/artigo3.df785af0.jpg"
+  },
+  {
+    "revision": "fd0ebde6e34a6631c57d",
+    "url": "/trabalho2/static/js/main.fd0ebde6.chunk.js"
+  },
+  {
+    "revision": "f0258797369cd9bd6b94b1c016470f04",
+    "url": "/trabalho2/static/media/fperfil.f0258797.jpg"
+  },
+  {
+    "revision": "e537824b0431f1f9a36696e84e64808f",
+    "url": "/trabalho2/static/media/git.e537824b.png"
+  },
+  {
+    "revision": "182282999e0300f3d935ca02aa259979",
+    "url": "/trabalho2/static/media/portfolio1.18228299.png"
+  },
+  {
+    "revision": "412215a0f1c3d16ab0b3c72e29eaea8c",
+    "url": "/trabalho2/static/media/curriculo.412215a0.pdf"
+  },
+  {
+    "revision": "6da1fa89f106006ae20af891f6b7a0f3",
+    "url": "/trabalho2/static/media/skill.6da1fa89.jpg"
+  },
+  {
+    "revision": "f3e51c69e622dbb25bbdbfb1fcd7c929",
+    "url": "/trabalho2/static/media/contato.f3e51c69.jpg"
+  },
+  {
+    "revision": "9adc3290e0344717fc602e7d0cd47b6e",
+    "url": "/trabalho2/index.html"
+  }
+];
